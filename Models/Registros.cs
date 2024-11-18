bede584f18a922809e88dee8c370fb878d146024
Alma Ozoria <heisylior@hotmail.com>
2024-11-17 23:09:10 -0400

    Creando carpeta models con el modelo

---
+++ Models/Registros.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlmaRosa_Ap1_P2.Models;
+
+public class Registros
+{
+    [Key]
+    public int RegistroId { get; set; }
+}
